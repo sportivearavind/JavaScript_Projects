3bb838e68630acdf181cab8907f5a61dbdb0dc60
Aravind <aravind16101800@gmail.com>
2024-07-21 21:58:23 +0530

    feat: simple header and gallery section created

---
+++ Firebase Auth/homepage.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC06I1XK-7Xe08wqIxbKYkM-4cLhFxNMT4",
+  authDomain: "firebasics-890e1.firebaseapp.com",
+  projectId: "firebasics-890e1",
+  storageBucket: "firebasics-890e1.appspot.com",
+  messagingSenderId: "520601722954",
+  appId: "1:520601722954:web:a22bbc558505948a2046b3",
+  measurementId: "G-9JW1F8M56B",
+};
+ 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const auth=getAuth();
+  const db=getFirestore();
+
+  onAuthStateChanged(auth, (user)=>{
+    const loggedInUserId=localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+        .then((docSnap)=>{
+            if(docSnap.exists()){
+                const userData=docSnap.data();
+                document.getElementById('loggedUserFName').innerText=userData.firstName;
+                document.getElementById('loggedUserEmail').innerText=userData.email;
+                document.getElementById('loggedUserLName').innerText=userData.lastName;
+
+            }
+            else{
+                console.log("no document found matching id")
+            }
+        })
+        .catch((error)=>{
+            console.log("Error getting document");
+        })
+    }
+    else{
+        console.log("User Id not Found in Local storage")
+    }
+  })
+
+  const logoutButton=document.getElementById('logout');
+
+  logoutButton.addEventListener('click',()=>{
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+    .then(()=>{
+        window.location.href='index.html';
+    })
+    .catch((error)=>{
+        console.error('Error Signing out:', error);
+    })
+  })

34f90d98c5a46a8cb1375e18cc81b3a7e2ad1277
Aravind <aravind16101800@gmail.com>
2024-07-27 13:59:54 +0530

    feat: counter section created

--- Vaidehi/app.js
+++ Vaidehi/app.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+  $(".count").counterUp({
+    delay:10,
+    time: 1200
+  });
+});
+
 document.querySelector("#contact-form").addEventListener("submit", (e) => {
   e.preventDefault();
   e.target.elements.name.value = "";
